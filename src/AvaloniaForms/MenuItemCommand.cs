1ebedf6b2299eb320d607c14f990700f40979bc0
zhongzf <zhongzf@126.com>
2020-02-09 17:42:22 +0800

    add more base controls.

---
+++ src/AvaloniaForms/MenuItemCommand.cs
@@ -0,0 +1,45 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace AvaloniaForms
+{
+	internal class MenuItemCommand : ICommand
+	{
+		readonly MenuItem _menuItem;
+
+		public MenuItemCommand(MenuItem item)
+		{
+			_menuItem = item;
+			_menuItem.PropertyChanged += OnElementPropertyChanged;
+		}
+
+		public virtual bool CanExecute(object parameter)
+		{
+			return _menuItem.IsEnabled;
+		}
+
+		public event EventHandler CanExecuteChanged;
+
+		public void Execute(object parameter)
+		{
+			//((IMenuItemController)_menuItem).Activate();
+		}
+
+		void OnCanExecuteChanged()
+		{
+			EventHandler changed = CanExecuteChanged;
+			if (changed != null)
+				changed(this, EventArgs.Empty);
+		}
+
+		void OnElementPropertyChanged(object sender, AvaloniaPropertyChangedEventArgs e)
+		{
+			if (e.Property.Name == InputElement.IsEnabledProperty.Name)
+				OnCanExecuteChanged();
+		}
+	}
+}
